c43a0005ec06ee7776bd13d7fad0896ec68cd2af
Your Name <jlmeeks587@gmail.com>
2023-01-24 12:09:57 -0500

    Title component created

--- src/index.js
+++ src/index.js
@@ -10,8 +10,13 @@ const BookList = () => {
 }
 
 const Book = () => {
-  return <article><Image /></article>;
+  return <article>
+    <Image />
+    <Title />
+  </article>;
 }
 const Image = () => <img src='https://images-na.ssl-images-amazon.com/images/I/51-UG7rgajL._AC_SX368_.jpg' width='150' alt=''/>
+
+const Title = () => <h1>A Random Walk Down Wall Street</h1>
 
 ReactDom.render(<BookList />, document.getElementById('root'))
