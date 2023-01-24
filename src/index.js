d9b0287c6d3e85bcf843ca2a8cc3b013d404f68e
Your Name <jlmeeks587@gmail.com>
2023-01-24 18:41:02 -0500

    Added data as props to components

--- src/index.js
+++ src/index.js
@@ -2,19 +2,40 @@ import React from "react";
 import ReactDom from 'react-dom';
 import './index.css';
 
+const firstBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/71aG+xDKSYL._AC_UL900_SR900,600_.jpg',
+  title: 'The 48 Laws of Power',
+  author: 'Robert Greene',
+}
+
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/71QKQ9mwV7L._AC_UL900_SR900,600_.jpg',
+  title: 'The Subtle Art of Not Giving a F*ck',
+  author: 'Mark Manson',
+}
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
+      <Book 
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book 
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   )
 }
 
-const Book = () => {
+const Book = (props) => {
   return <article className="book">
-    <img src='https://images-na.ssl-images-amazon.com/images/I/51-UG7rgajL._AC_SX368_.jpg' width='150' alt=''/>
-    <h1>A Random Walk Down Wall Street</h1>
-    <h4>Burton G. Malkiel</h4>
+    <img src={props.img} width='200px' alt=''/>
+    <h1>{props.title}</h1>
+    <h4>{props.author}</h4>
   </article>;
 }
 
