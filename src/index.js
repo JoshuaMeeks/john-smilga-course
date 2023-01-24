3ebb34c7b5c6e354d71652e80dca03c6c6050c9f
Your Name <jlmeeks587@gmail.com>
2023-01-24 12:12:00 -0500

    Author component created

--- src/index.js
+++ src/index.js
@@ -13,10 +13,12 @@ const Book = () => {
   return <article>
     <Image />
     <Title />
+    <Author />
   </article>;
 }
 const Image = () => <img src='https://images-na.ssl-images-amazon.com/images/I/51-UG7rgajL._AC_SX368_.jpg' width='150' alt=''/>
 
 const Title = () => <h1>A Random Walk Down Wall Street</h1>
 
+const Author = () => <h4>Burton G. Malkiel</h4>
 ReactDom.render(<BookList />, document.getElementById('root'))
