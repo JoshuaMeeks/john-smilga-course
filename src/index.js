d31fce4dfc6445fa3cf53d0dcce1fb9867762796
Your Name <jlmeeks587@gmail.com>
2023-01-24 15:23:37 -0500

    Inline CSS styling

--- src/index.js
+++ src/index.js
@@ -25,8 +25,7 @@ const Book = () => {
   </article>;
 }
 const Image = () => <img src='https://images-na.ssl-images-amazon.com/images/I/51-UG7rgajL._AC_SX368_.jpg' width='150' alt=''/>
-
 const Title = () => <h1>A Random Walk Down Wall Street</h1>
+const Author = () => <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Burton G. Malkiel</h4>
 
-const Author = () => <h4>Burton G. Malkiel</h4>
 ReactDom.render(<BookList />, document.getElementById('root'))
